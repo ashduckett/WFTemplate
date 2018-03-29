8ca842ccd18b92965f2aa2f1ddef9661beee337b
Ash Duckett <ash.duckett@outlook.com>
2018-03-29 18:23:06 +0100

    Text added to scroller

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
     function createGenericOverlay() {
         var overlay = document.createElement('div');
+        /* Get as much of this out and into CSS as possible */
         overlay.style.position = 'absolute';
         overlay.style.top = '0';
         overlay.style.left = '0';
@@ -9,7 +10,24 @@ $(document).ready(function() {
         overlay.style.height = '100%';
         overlay.style.backgroundPosition = 'center';
         overlay.style.backgroundRepeat = 'no-repeat';
+        overlay.style.fontFamily = 'semplicita-medium';
         return overlay;
+    }
+
+    function createTextOverlay(text) {
+        var textOverlay = createGenericOverlay();
+
+        var textElement = document.createElement('div');
+        textElement.innerText = text;
+        textElement.style.color = 'white';
+        textElement.style.position = 'relative';
+        textElement.style.top = '70%';
+        textElement.style.textAlign = 'center';
+        textElement.style.marginTop = '5px';
+        textElement.style.letterSpacing = '0.3em';
+
+        textOverlay.appendChild(textElement);
+        return textOverlay;
     }
 
     function createOpacityOverlay() {
@@ -17,6 +35,9 @@ $(document).ready(function() {
         opacityBg.style.backgroundColor = 'black';
         opacityBg.style.backgroundSize = '50%';
         opacityBg.style.opacity = '0.5';
+
+        
+
         return opacityBg;
     }
 
@@ -29,10 +50,10 @@ $(document).ready(function() {
 
     $('.slider').slider('£', 50, 10000);
 
-    var scrollElementOne = new ScrollElement('scroller1 - Copy.jpeg', [createOpacityOverlay(), createImageOverlay('no1.png')]);
-    var scrollElementTwo = new ScrollElement('scroller2.jpeg', [createOpacityOverlay(), createImageOverlay('no2.png')]);
-    var scrollElementThree = new ScrollElement('scroller3.jpeg', [createOpacityOverlay(), createImageOverlay('no3.png')]);
-    var scrollElementFour = new ScrollElement('scroller4.jpeg', [createOpacityOverlay(), createImageOverlay('no4.png')]);
+    var scrollElementOne = new ScrollElement('scroller1 - Copy.jpeg', [createOpacityOverlay(), createImageOverlay('no1.png'), createTextOverlay('LVT')]);
+    var scrollElementTwo = new ScrollElement('scroller2.jpeg', [createOpacityOverlay(), createImageOverlay('no2.png'), createTextOverlay('WOOD')]);
+    var scrollElementThree = new ScrollElement('scroller3.jpeg', [createOpacityOverlay(), createImageOverlay('no3.png'), createTextOverlay('VINYL')]);
+    var scrollElementFour = new ScrollElement('scroller4.jpeg', [createOpacityOverlay(), createImageOverlay('no4.png'), createTextOverlay('CARPET')]);
 
     $('.scroller').scroller([scrollElementOne, scrollElementTwo, scrollElementThree, scrollElementFour]);
     $('.combo-box.business-type').combo('HOME', ['Commercial', 'Home']);
